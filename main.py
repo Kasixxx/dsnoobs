e3cb75f42ff4baf027e9ee3ab53d145703940ddf
Kasia <14kasia14@gmail.com>
2022-11-19 20:11:20 +0100

    sth

--- main.py
+++ main.py
@@ -1,31 +1,19 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-# Use a breakpoint in the code line below to debug your script.
-# Press the green button in the gutter to run the script.
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
 import tensorflow as tf
 import tensorflow.python.keras as keras
 import pandas as pd
 import numpy as np
+import scipy
 import os
 import shutil
 import matplotlib.pyplot as plt
 
 from keras import layers
 
+from keras.utils.np_utils import to_categorical
+
 from pathlib import Path
 
-data_dir = "/Users/14kas/PycharmProjects/pythonProject1/train_test_data/train"
-
-
-def split_data_set(dataset):
-    for images_df, labels_df in dataset.take(1):
-        numpy_images = images_df.numpy()
-        numpy_labels = labels_df.numpy()
-        return numpy_images, numpy_labels
+from keras.preprocessing.image import ImageDataGenerator
 
 
 def add_to_class_directories(label, image_path, class_directory_path):
@@ -43,35 +31,50 @@ def create_class_directories(csv_path, class_directory_path):
         add_to_class_directories(label, path, class_directory_path)
 
 
-create_class_directories("train.csv", "train_test_data/train")
+data_dir = "train_test_data/train"
 
-image_df_train, image_df_validation = tf.keras.utils.image_dataset_from_directory(
-    data_dir,
-    seed=2137,
-    validation_split=0.2,
-    subset="both",
-    image_size=(332, 332)
+create_class_directories("train.csv", data_dir)
+
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True
 )
 
+train_generator = train_datagen.flow_from_directory(
+    data_dir,
+    target_size=(322, 322),
+    batch_size=32,
+    class_mode='categorical')
+
 model = keras.Sequential([
+    layers.Resizing(83, 83),
+    layers.Conv2D(64, 7, padding='same', activation='relu'),
+    layers.MaxPooling2D(2),
+    layers.Conv2D(128, 5, padding='same', activation='relu'),
+    layers.Conv2D(128, 5, padding='same', activation='relu'),
+    layers.MaxPooling2D(2),
+    layers.Conv2D(256, 5, padding='same', activation='relu'),
+    layers.Conv2D(256, 5, padding='same', activation='relu'),
+    layers.MaxPooling2D(2),
     layers.Flatten(),
-    layers.Normalization(),
-    layers.Dense(256, activation='relu', input_shape=(332 * 332,)),
     layers.Dense(128, activation='relu'),
-    layers.Dense(10, activation='softmax'),
+    layers.Dropout(0.5),
+    layers.Dense(64, activation='relu'),
+    layers.Dropout(0.5),
+    layers.Dense(32, activation='relu'),
+    layers.Dense(3, activation='softmax')
 ])
 
 model.compile(
     optimizer='adam',
     loss='categorical_crossentropy',
-    metrics=['accuracy'],
+    metrics=['accuracy']
 )
 
-images, labels = split_data_set(image_df_train)
-
-model.fit(
-    images,
-    labels,
-    epochs=5,
-    batch_size=30,
+model.fit_generator(
+    train_generator,
+    epochs=10,
+    steps_per_epoch=43
 )
